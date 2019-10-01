f5c6fc1ba3e0de465bcb3fe7608a67e059f6d38d
gitlab@evertheme.com <gitlab@evertheme.com>
2019-10-01 17:05:17 -0500

    update fonts

--- moveAssets.js
+++ moveAssets.js
@@ -7,19 +7,18 @@ const fs = require('fs');
   const dirName = './client/build/static/css/';
   const cssFile = './client/src/theme.css';
   await fsx.copy(src, dist);
-  console.log('media copied');
+  // console.log('media copied');
 
   fs.readdir(dirName, (err, files) => {
     if (err) console.log(err);
     let fileName;
     files.forEach(file => {
-      console.log(file);
+      // console.log(file);
       if (file.indexOf('main.') > -1 && file.indexOf('.map') < 0) {
         fileName = dirName + file;
-        console.log('fileName: ', fileName);
+        // console.log('fileName: ', fileName);
         fsx.copy(cssFile, fileName);
       }
     });
-    // await fsx.copy(cssFile, filename);
   });
 })();
