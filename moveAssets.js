73bc623c06dbf7df205072197f6a1260e78694ed
gitlab@evertheme.com <gitlab@evertheme.com>
2019-10-07 13:15:57 -0500

    update move assets

--- moveAssets.js
+++ moveAssets.js
@@ -4,21 +4,5 @@ const fs = require('fs');
 (async () => {
   const src = './client/public/static/media';
   const dist = './client/build/static/media';
-  const dirName = './client/build/static/css/';
-  const cssFile = './client/src/theme.css';
   await fsx.copy(src, dist);
-  // console.log('media copied');
-
-  fs.readdir(dirName, (err, files) => {
-    if (err) console.log(err);
-    let fileName;
-    files.forEach(file => {
-      // console.log(file);
-      if (file.indexOf('main.') > -1 && file.indexOf('.map') < 0) {
-        fileName = dirName + file;
-        // console.log('fileName: ', fileName);
-        fsx.copy(cssFile, fileName);
-      }
-    });
-  });
 })();
