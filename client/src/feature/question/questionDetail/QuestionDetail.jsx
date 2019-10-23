2534e227dbcd490863f7585882b1fa3836934139
gitlab@evertheme.com <gitlab@evertheme.com>
2019-10-23 12:01:26 -0500

    fix question detail

--- client/src/feature/question/questionDetail/QuestionDetail.jsx
+++ client/src/feature/question/questionDetail/QuestionDetail.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Button, Icon } from 'semantic-ui-react';
+import moment from 'moment/moment.js';
+import PageLoader from '../../../app/layout/PageLoader';
 import { loadQuestionBySlug, loadQuestionById } from '../questionActions';
 
 const mapState = state => ({
+  loading: state.async.loading,
   questions: state.questions.questions,
   currentQuestion: state.questions.currentQuestion
 });
@@ -18,6 +21,7 @@ export class QuestionDetail extends Component {
     const {
       match: { params }
     } = this.props;
+    console.log('componentDidMount:', params.uid, params.slug);
 
     if (params.uid && params.slug) {
       this.props.loadQuestionBySlug(params.uid, params.slug);
@@ -28,7 +32,44 @@ export class QuestionDetail extends Component {
   }
 
   render() {
-    return <div>question detail</div>;
+    const { currentQuestion, loading } = this.props;
+    if (loading) return <PageLoader />;
+    return (
+      <>
+        {currentQuestion && (
+          <div className='question-detail'>
+            <div className='flex-wrap sm'>
+              <div className='grow'>
+                <h1 className='mb-1'>{currentQuestion.title}</h1>
+                <p>
+                  <span>Asked</span>&nbsp;{moment(currentQuestion.updated).from()}
+                  <span> &nbsp;Active</span>&nbsp;{moment(currentQuestion.updated).from()}
+                  <span> &nbsp;Viewed</span>&nbsp;{currentQuestion.viewCount}
+                </p>
+              </div>
+              <div>
+                <Button color='green'>Ask Question</Button>
+              </div>
+            </div>
+            <hr />
+            <div className='flex-wrap'>
+              <div className='text-center pr-3'>
+                <Icon name='caret up' color='grey' size='big' className='mr-0 pt-0'></Icon>
+                <div>5</div>
+                <Icon name='caret down' color='grey' size='big' className='mr-0'></Icon>
+              </div>
+              <div className='grow mb-2' dangerouslySetInnerHTML={{ __html: currentQuestion.content }} />
+            </div>
+            <hr />
+            <h5 className='my-1'>
+              {currentQuestion.answers.length} Answer{currentQuestion.answers.length === 1 ? '' : 's'}
+            </h5>
+            <hr />
+            {currentQuestion.answers && currentQuestion.answers.length > 0 && currentQuestion.answers.map(answer => <div key={answer._id}>answers</div>)}
+          </div>
+        )}
+      </>
+    );
   }
 }
 
