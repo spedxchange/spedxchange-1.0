f8418136b4b9011400b0a2f725da088a45efeb76
gitlab@evertheme.com <gitlab@evertheme.com>
2019-10-25 10:03:00 -0500

    fix fonts

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.use('/api/roles', require('./routes/api/roles'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+  app.use('/client/build/static', express.static(path.join(__dirname, 'client/build/static')));
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
