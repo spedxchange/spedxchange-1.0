ea48968d1409f06acb01cb5adb1958c813f4ad11
gitlab@evertheme.com <gitlab@evertheme.com>
2019-10-29 03:22:32 -0500

    update question styles

--- client/src/feature/question/questionDetail/QuestionDetail.jsx
+++ client/src/feature/question/questionDetail/QuestionDetail.jsx
@@ -30,44 +30,42 @@ class QuestionDetail extends Component {
       <>
         {question && (
           <div className='question-detail'>
-            <div className='flex-box sm'>
-              <div className='grow'>
+            <div className='flex-box md'>
+              <div className='grow info'>
                 <h1 className='mb-1'>{question.title}</h1>
                 <p>
-                  <span>Asked</span>&nbsp;{moment(question.updated).from()}
-                  <span> &nbsp;Active</span>&nbsp;{moment(question.updated).from()}
-                  <span> &nbsp;Viewed</span>&nbsp;{question.viewCount}
+                  <span>Asked</span>&nbsp;<strong>{moment(question.updated).from()}</strong>
                 </p>
+                <hr />
+                <div className='flex-box'>
+                  <div className='text-center pr-3'>
+                    <Icon name='caret up' color='grey' size='big' className='mr-0 pt-0'></Icon>
+                    <div>5</div>
+                    <Icon name='caret down' color='grey' size='big' className='mr-0'></Icon>
+                  </div>
+                  <div className='grow mb-2' dangerouslySetInnerHTML={{ __html: question.content }} />
+                </div>
+                <hr />
+                <h5 className='my-1'>
+                  {question.answers ? question.answers.length : 0} Answer{question.answers.length === 1 ? '' : 's'}
+                </h5>
+                <hr />
+                {question.answers &&
+                  question.answers.length > 0 &&
+                  question.answers.map(answer => (
+                    <div key={answer._id}>
+                      <div className='mb-3' dangerouslySetInnerHTML={{ __html: answer.content }} />
+                      <hr />
+                    </div>
+                  ))}
+                {auth.authenticated && <AnswerForm question={question} reload={loadQuestionBySlugAsView} />}
               </div>
-              <div>
+              <div className='ask-button'>
                 <Button color='green' onClick={() => history.push('/ask')}>
                   Ask Question
                 </Button>
               </div>
             </div>
-            <hr />
-            <div className='flex-box'>
-              <div className='text-center pr-3'>
-                <Icon name='caret up' color='grey' size='big' className='mr-0 pt-0'></Icon>
-                <div>5</div>
-                <Icon name='caret down' color='grey' size='big' className='mr-0'></Icon>
-              </div>
-              <div className='grow mb-2' dangerouslySetInnerHTML={{ __html: question.content }} />
-            </div>
-            <hr />
-            <h5 className='my-1'>
-              {question.answers ? question.answers.length : 0} Answer{question.answers.length === 1 ? '' : 's'}
-            </h5>
-            <hr />
-            {question.answers &&
-              question.answers.length > 0 &&
-              question.answers.map(answer => (
-                <div key={answer._id}>
-                  <div className='mb-3' dangerouslySetInnerHTML={{ __html: answer.content }} />
-                  <hr />
-                </div>
-              ))}
-            {auth.authenticated && <AnswerForm question={question} reload={loadQuestionBySlugAsView} />}
           </div>
         )}
       </>
